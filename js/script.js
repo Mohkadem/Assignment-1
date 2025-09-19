d0a633eddb23b2937ccbbe166e18162c729c53e2
Mohkadem <s202245820@kfupm.edu.sa>
2025-09-19 21:25:51 +0300

    Main page is done + modifying header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-let header = document.querySelector('.header');
+let header = document.querySelector('header');
+let listElements = document.querySelectorAll('a')
 window.addEventListener('scroll', () => {
     let height = window.scrollY
 
     if (height >= 40) {
-        header.style.backgroundColor = "rgb(211, 211, 211, 0.3)" 
-        header.style.boxShadow = '0 2px 6px rgba(0,0,0,0.2)';
+        header.style.backgroundColor = "rgb(211, 211, 211)" 
+        header.style.boxShadow = '0 2px 6px rgba(0,0,0, 0.5)';
+        header.style.color = "#333"
+        listElements.forEach(el => {
+            el.style.color = "#333"
+        })
     }
     else {
         header.style.backgroundColor = "" 
         header.style.boxShadow = '';
-    
+        header.style.color = ""
+        listElements.forEach(el => {
+            el.style.color = ""
+        })
     }
 })
